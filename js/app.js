f1f27946bedf3aeb2fb4a64ab79474b45d5aabcb
Andrew Abalos <andysaurusrex21@gmail.com>
2018-01-25 05:49:07 -0800

    fixed landing page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
 // Scroll animation jQuery
 
-$(document).ready(function() {
-  var scrollLink = '.scrolls';
-
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top()
-    });
-  });
-});
-
 // Type animation
 
   var typed = new Typed('#typed', {
